7273830c99f6626e2da7e09e1e3a7a4ef6b87854
PIJUSH364 <praymondal@gmail.com>
2022-12-31 00:00:17 +0530

    all filter functionality done

--- src/components/common/EmptyView/EmptyViewPage.jsx
+++ src/components/common/EmptyView/EmptyViewPage.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Box, Stack } from '@mui/material';
+import React from 'react';
 
 export const EmptyViewPage = () => {
   return (
-    <div>EmptyViewPage</div>
-  )
-}
+    <Stack
+      className="emptyView-warp"
+      height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center">
+      <img
+        src="/images/gif/empty.gif"
+        alt="emptyView"
+        style={{
+          maxWidth: '500px',
+          width: '100%',
+        }}
+      />
+    </Stack>
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import { Stack } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { EmptyViewPage } from '../../components/common/EmptyView/EmptyViewPage';
 import { FilterPanel } from '../../components/Home/FilterPanel/FilterPanel';
 import { List } from '../../components/Home/List/List';
@@ -8,6 +8,8 @@ import { SearchBar } from '../../components/Home/searchBar/SearchBar';
 import { dataList } from '../../Constants/data';
 
 export const Home = () => {
+  const [inputSearch, setInputSearch] = useState('');
+  const [resultFound, setResultFound] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedRating, setSelectedRating] = useState(null);
   const [selectedPrice, setSelectedPrice] = useState([1000, 5000]);
@@ -35,14 +37,65 @@ export const Home = () => {
     setSelectedPrice(value);
   };
 
-// filter functionality 
+  // filter functionality
+  const applyFilter = () => {
+    let updatedList = dataList;
 
+    //rating filter
+    if (selectedRating) {
+      updatedList = updatedList.filter(
+        (item) => parseInt(item.rating) === parseInt(selectedRating)
+      );
+    }
 
+    //category filter
+    if (selectedCategory) {
+      updatedList = updatedList.filter(
+        (item) => item.category === selectedCategory
+      );
+    }
+    //cuisine filter
+    const cuisineChecked = cuisines
+      .filter((item) => item.checked)
+      .map((item) => item.label.toLocaleLowerCase());
+
+    if (cuisineChecked.length) {
+      updatedList = updatedList.filter((item) =>
+        cuisineChecked.includes(item.cuisine)
+      );
+    }
+
+    // price filter
+    const minPrice = selectedPrice[0];
+    const maxPrice = selectedPrice[1];
+    updatedList = updatedList.filter(
+      (item) => item.price >= minPrice && item.price <= maxPrice
+    );
+
+    // search filter
+    if (inputSearch) {
+      updatedList = updatedList.filter(
+        (item) =>
+          item.title
+            .toLowerCase()
+            .search(inputSearch.toLocaleLowerCase().trim()) !== -1
+      );
+    }
+    // finally list of  item set
+    setList(updatedList);
+    !updatedList.length ? setResultFound(false) : setResultFound(true);
+  };
+  useEffect(() => {
+    applyFilter();
+  }, [selectedRating, selectedCategory, cuisines, selectedPrice, inputSearch]);
 
   return (
     <Stack className="home" direction="column" height="100vh">
       {/* search bar */}
-      <SearchBar />
+      <SearchBar
+        value={inputSearch}
+        changeInput={(e) => setInputSearch(e.target.value)}
+      />
       <Stack
         className="home_panelList-wrap"
         padding="1rem"
@@ -68,8 +121,9 @@ export const Home = () => {
         </Box>
         <Box className="home_panel-wrap" flex={1} overflow="auto">
           {/* list and empty view */}
-          <List list={list} />
-          {/* <EmptyViewPage/> */}
+          {setResultFound ? <List list={list} /> : <EmptyViewPage />}
+
+          <EmptyViewPage />
         </Box>
       </Stack>
     </Stack>
