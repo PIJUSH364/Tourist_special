68a331cf014ddcfe2d4ce706e70e31c31ced95ac
PIJUSH364 <praymondal@gmail.com>
2023-01-02 16:13:43 +0530

    all functionality done

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import {
   Cancel,
+  Clear,
   Close,
   ExpandMore,
   FilterList,
@@ -61,13 +62,16 @@ export const NavBar = ({
   return (
     <>
       <Stack
+        fontFamily={"font-family: 'Raleway', sans-serif"}
         direction="row"
         gap={2}
         p={2}
         justifyContent="space-between"
         boxShadow="rgb(50 50 93 / 25%) 0px 50px 100px -20px, rgb(0 0 0 / 30%) 0px 8px 9px -30px">
         <Box>
-          <Typography variant="h5">Tourist_choice</Typography>
+          <Typography variant="h4" component={'h2'}>
+            Tourist_choice
+          </Typography>
         </Box>
         <Box direction="row" gap={2} alignItems="center" sx={menuItemStyle}>
           <Typography variant="h6">LogIn</Typography>
@@ -86,75 +90,85 @@ export const NavBar = ({
 
       <Box>
         <Stack
+          justifyContent={'flex-end'}
+          direction="row"
           gap={1}
-          boxShadow={
-            ' rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(48, 41, 41, 0.8) -6px -2px 16px 0px'
-          }
-          bgcolor={'#fff'}
           color={'#000'}
           style={{
             top: 0,
             bottom: 0,
             right: 0,
-            width: '300px',
+            width: '370px',
             position: 'fixed',
             overflow: 'auto',
             transition: ' all 1.3s ease-in-out',
             transform: transformValue,
           }}>
-          <Stack
-            direction="row"
-            justifyContent={'space-between'}
-            alignItems={'center'}>
-            <Stack direction="row" alignItems={'center'} p={0.5}>
-              <IconButton>
-                <PersonPin sx={{ fontSize: 35 }} />
-              </IconButton>
-              <Typography variant="h5" component={'p'} color={'#2979FF'}>
-                LogIn / SignUp
-              </Typography>
-            </Stack>
-
-            <div>
-              <IconButton
+          {' '}
+          <Box height={'fitContent'} pt={1}>
+            <IconButton>
+              <Clear
+                sx={{ fontSize: 40, color: '#fff' }}
                 onPointerDown={() => {
                   setToggleStatus(!toggleStatus);
                   setTimeout(() => {
                     dispatch(changeZIndex(3));
                   }, 1000);
-                }}>
-                <Cancel sx={{ fontSize: 32 }} />
-              </IconButton>
-            </div>
-          </Stack>
-          <Divider />
-          <Box p={1}>
-            <Accordion>
-              <AccordionSummary
-                onClick={() => {
-                  setFilterIsOpen(!filterIsOpen);
                 }}
-                aria-controls="panel1d-content"
-                id="panel1d-header">
-                <IconButton>
-                  {filterIsOpen ? <ExpandMore /> : <FilterList />}
-                </IconButton>
-                <Typography>Filter Panel</Typography>
-              </AccordionSummary>{' '}
-              <AccordionDetails>
-                <FilterPanel
-                  cuisines={cuisines}
-                  selectedCategory={selectedCategory}
-                  selectedRating={selectedRating}
-                  selectedPrice={selectedPrice}
-                  selectToggle={handleSelectCategory}
-                  changePrice={handleChangePrice}
-                  changeChecked={handleChangeChecked}
-                  selectRating={handleSelectRating}
-                />
-              </AccordionDetails>
-            </Accordion>
+              />
+            </IconButton>
           </Box>
+          <Stack
+            pt={2}
+            bgcolor={'#fff'}
+            boxShadow={
+              ' rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(48, 41, 41, 0.8) -6px -2px 16px 0px'
+            }>
+            <Stack
+              direction="row"
+              justifyContent={'space-between'}
+              alignItems={'center'}>
+              <Stack direction="row" alignItems={'center'} p={0.5}>
+                <IconButton>
+                  <PersonPin sx={{ fontSize: 35 }} />
+                </IconButton>
+                <Typography variant="h5" component={'p'} color={'#2979FF'}>
+                  LogIn / SignUp
+                </Typography>
+              </Stack>
+            </Stack>
+            <Divider />
+            <Box p={1}>
+              <Accordion>
+                <AccordionSummary
+                  sx={{
+                    alignItems: 'center',
+                  }}
+                  onClick={() => {
+                    setFilterIsOpen(!filterIsOpen);
+                  }}
+                  aria-controls="panel1d-content"
+                  id="panel1d-header">
+                  <Typography>Filter Panel</Typography>
+                  <IconButton>
+                    {filterIsOpen ? <ExpandMore /> : <FilterList />}
+                  </IconButton>
+                </AccordionSummary>{' '}
+                <AccordionDetails>
+                  <FilterPanel
+                    cuisines={cuisines}
+                    selectedCategory={selectedCategory}
+                    selectedRating={selectedRating}
+                    selectedPrice={selectedPrice}
+                    selectToggle={handleSelectCategory}
+                    changePrice={handleChangePrice}
+                    changeChecked={handleChangeChecked}
+                    selectRating={handleSelectRating}
+                  />
+                </AccordionDetails>
+              </Accordion>
+            </Box>
+          </Stack>
         </Stack>
       </Box>
     </>
